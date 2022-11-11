97c20bc7422217d322e0b9ca02474f0df0ab6012
funzter <funzter@mail.timesnake.de>
2022-11-11 20:31:16 +0100

    add logger to plugins, add log command

--- src/main/java/de/timesnake/game/story/chat/Plugin.java
+++ src/main/java/de/timesnake/game/story/chat/Plugin.java
@@ -1,5 +1,5 @@
 /*
- * game-story.main
+ * timesnake.game-story.main
  * Copyright (C) 2022 timesnake
  *
  * This program is free software; you can redistribute it and/or
@@ -18,11 +18,16 @@
 
 package de.timesnake.game.story.chat;
 
+import de.timesnake.library.basic.util.LogHelper;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Plugin extends de.timesnake.basic.bukkit.util.chat.Plugin {
 
-    public static final Plugin STORY = new Plugin("Story", "GSY");
+    public static final Plugin STORY = new Plugin("Story", "GSY", LogHelper.getLogger("Story", Level.INFO));
 
-    protected Plugin(String name, String code) {
-        super(name, code);
+    protected Plugin(String name, String code, Logger logger) {
+        super(name, code, logger);
     }
 }
